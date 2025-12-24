a96baa028cd9e01517b032cb1f77e096e736cd02
Alexander Kleinhans <alexanderkleinhans234@gmail.com>
2025-12-23 20:44:34 -0800

    hn done

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # /demo/ch
 def index():
     return render_template("index.html")
+
+@app.route("/demo/hn")
+def demo_hn():
+    return render_template("demo-hn.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
